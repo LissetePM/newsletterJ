e985e24d8d8bc4ec23b43a1edca7e04380a5fb64
Lissete Plata <lisseteplatam@gmail.com>
2022-06-13 07:00:22 -0500

    Correction for php7

--- controller/conexion.php
+++ controller/conexion.php
@@ -5,11 +5,15 @@ $username = "dbmaster";
 $password = "passnewsletter57";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,'newsletter');
-
+try{
+  $conn = new mysqli($servername, $username, $password,'newsletter');
+}catch(mysqli_sql_exception $e){
+  header('Location: ../static/error.html');
+}
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+  header('Location: ../static/error.html');
 }
 //echo "Connected successfully";
 ?>
--- controller/validation.php
+++ controller/validation.php
@@ -1,29 +1,26 @@
 <?php
+
 include 'conexion.php';
 $error_duplicateEntry = 'Duplicate entry';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // collect value of input field
   $email = $_POST['email'];
-  if (empty($email)) {
-    echo "Email is empty";
-  } 
-  else {
-    try {
-      $insertar = "INSERT INTO Email(Email) VALUES ('$email')";
-      $query= mysqli_query($conn, $insertar);
-
-      if($query == 1)
-        header('Location: ../static/finishregister.html');
-      else
-      echo "$query";
-    } catch (mysqli_sql_exception $e) {
-      if(strpos($e, $error_duplicateEntry, 0)){
-          header('Location: ../static/mailregistered.html');
-      }        
-      else{
-        header('Location: ../static/error.html');
-      }
+  try {
+    $insertar = "INSERT INTO Email(Email) VALUES ('$email')";
+    $query= mysqli_query($conn, $insertar);
+    if($query == 1)
+      header('Location: ../static/finishregister.html');
+    else
+      header('Location: ../static/mailregistered.html');
+    }
+  catch (mysqli_sql_exception $e) {
+    if(strpos($e, $error_duplicateEntry, 0)){
+        header('Location: ../static/mailregistered.html');
+    }        
+    else{
+      header('Location: ../static/error.html');
     }
   }
+  
 }
 ?>
