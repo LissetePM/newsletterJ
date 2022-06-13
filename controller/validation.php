3052dc69e2bffd8e81fc1d4d74f88764f27ddeaf
Lissete Plata <lisseteplatam@gmail.com>
2022-06-13 06:04:36 -0500

    Validation

---
+++ controller/validation.php
@@ -0,0 +1,25 @@
+<?php
+include 'conexion.php';
+$error_duplicateEntry = 'Duplicate entry';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $email = $_POST['email'];
+  if (empty($email)) {
+    echo "Email is empty";
+  } 
+  else {
+    try {
+      $insertar = "INSERT INTO Email(Email) VALUES ('$email')";
+      $query= mysqli_query($conn, $insertar);
+      header('Location: ../static/finishregister.html');
+    } catch (mysqli_sql_exception $e) {
+      if(strpos($e, $error_duplicateEntry, 0)){
+          header('Location: ../static/mailregistered.html');
+      }        
+      else{
+        header('Location: ../static/error.html');
+      }
+    }
+  }
+}
+?>
